53c3e40761ff8b6b1cc85cdd0735a575fcf66ebb
Peter Harry <prharry@hotmail.com>
2026-08-06 19:16:06 +1000

    Try to print category

--- custom_components/solar_of_things/api.py
+++ custom_components/solar_of_things/api.py
@@ -696,16 +696,17 @@ class SolarOfThingsAPI:
         my_category = my_data.get("category")
         my_prop = my_data.get("properties")
         my_list = my_data.get("list")
-        if my_prop is not None:
-          print(f"solar_of_things - properties not None")
+        #if my_prop is not None:
+          #print(f"solar_of_things - properties not None")
+          
           #my_category = data.get("category")
         #if my_category is not None:
           #my_id = my_data.get("id")
         #_LOGGER.debug("solar_of_things: my_key %s", my_id)
         #print(f"solar_of_things - key = {my_id}")
-        my_properties = data.get("properties")
-        if my_properties is not None:
-          print(f"solar_of_things - properties not None")
+        
+        if my_category is not None:
+          print(f"solar_of_things - categories {my_category}")
 
         props = (((data.get("data") or {}).get("properties")) or
                  (data.get("data") or {}).get("list") or
