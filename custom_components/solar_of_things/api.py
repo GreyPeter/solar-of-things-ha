9e9912abc219bedd74b5a43bc2161486e467baf0
Peter Harry <prharry@hotmail.com>
2026-08-06 15:02:28 +1000

    Revert "Switch back to print"
    This reverts commit ba91e6ddbef5f15d3082e0c4a70f8dcda39caf38.

--- custom_components/solar_of_things/api.py
+++ custom_components/solar_of_things/api.py
@@ -698,7 +698,7 @@ class SolarOfThingsAPI:
 
         result: dict[str, Any] = {}
         for item in props if isinstance(props, list) else []:
-        print("key = ",k)
+        log.info("key = ",k)
             k = item.get("key") or item.get("name")
             v = item.get("value")
             if k and v is not None:
