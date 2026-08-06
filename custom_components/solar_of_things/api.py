6c97367a30465451615f8811f6c445df6d41aa2c
Peter Harry <prharry@hotmail.com>
2026-08-06 15:03:06 +1000

    Revert "data.get(code)"
    This reverts commit 1623c3bce1a9d3a27f2971bb7713f49bdc7c64a7.

--- custom_components/solar_of_things/api.py
+++ custom_components/solar_of_things/api.py
@@ -684,7 +684,7 @@ class SolarOfThingsAPI:
         resp.raise_for_status()
         
         data = resp.json()
-        log.info("data = ",data.get("code"))
+        log.info("data = ",data)
 
         if data.get("code") not in (0, None):
             raise RuntimeError(
