389367b67eb318442de8d0574f4283615b9b1db1
Peter Harry <prharry@hotmail.com>
2026-08-08 10:39:24 +1000

    Try to fix error

--- custom_components/solar_of_things/api.py
+++ custom_components/solar_of_things/api.py
@@ -702,7 +702,7 @@ class SolarOfThingsAPI:
         result: dict[str, Any] = {}
         for item in props if isinstance(props, list) else []:
             k = item.get("key") or item.get("name")
-            timePoints = item.get("timePoints") or {})get("time")
+            timePoints = (((item.get("timePoints") or {}).get("time")) or[])
             #august = timePoints.get("time")
             property = item.get("property")
             propertyKey = property.get("key")
