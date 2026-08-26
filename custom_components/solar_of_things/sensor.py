8e7d232d56356fd935b8fbcf224cd30499ff5213
Peter Harry <prharry@hotmail.com>
2026-08-26 19:10:12 +1000

    Return to last

--- custom_components/solar_of_things/sensor.py
+++ custom_components/solar_of_things/sensor.py
@@ -176,8 +176,8 @@ class SolarOfThingsDeviceSensor(CoordinatorEntity, SensorEntity):
 
     @property
     def native_value(self):
-        #ts = (self.coordinator.data or {}).get("time_series") or {}
-        ts = (self.coordinator.data or {}).get("owner") or {}
+        ts = (self.coordinator.data or {}).get("time_series") or {}
+        #ts = (self.coordinator.data or {}).get("owner") or {}
         val = ts.get(self._sensor_key)
         if val is None:
             return None
