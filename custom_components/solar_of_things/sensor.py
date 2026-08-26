fe60ce2c0c928b25b6ad4b4629ec88d330f745d1
Peter Harry <prharry@hotmail.com>
2026-08-26 18:34:11 +1000

    Try processing Owners data

--- custom_components/solar_of_things/sensor.py
+++ custom_components/solar_of_things/sensor.py
@@ -93,6 +93,23 @@ async def async_setup_entry(
                 )
             )
 
+    # Station-level monthly sensors
+    if station_coordinator:
+        for key, definition in SENSOR_DEFINITIONS.items():
+            if not key.endswith("ProducedQuantity"):
+                continue
+
+            entities.append(
+                SolarOfThingsDeviceSensor(
+                    coordinator=coordinator,
+                    station_id=station_id,
+                    device_id=device_id,
+                    device_name=device_name,
+                    sensor_key=key,
+                    sensor_definition=definition,
+                )
+            )
+
     async_add_entities(entities)
 
 class SolarOfThingsDeviceSensor(CoordinatorEntity, SensorEntity):
