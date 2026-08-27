c47da2ae01093b031922a759b1f8e1ccf1cf7f35
Peter Harry <prharry@hotmail.com>
2026-08-27 13:05:04 +1000

    Debug SolarOfThingsOwnerSensor

--- custom_components/solar_of_things/sensor.py
+++ custom_components/solar_of_things/sensor.py
@@ -259,7 +259,7 @@ class SolarOfThingsOwnerSensor(CoordinatorEntity, SensorEntity):
         self._device_name = device_name
         self._sensor_key = sensor_key
         self._sensor_definition = sensor_definition
-        #_LOGGER.info("SolarOfThingsOwnerSensor - sensor defination %s",sensor_definition)
+        _LOGGER.info("SolarOfThingsOwnerSensor - coordinator.data %s",self.coordinator.data)
 
         self._attr_has_entity_name = True
         self._attr_translation_key = _TRANSLATION_KEYS.get(sensor_key)
